91986bdbba316617348a5b399cee49433394e842
TsubasaHiga <info@higatsubasa.com>
2021-07-22 15:01:55 +0900

    update:#144 getOrientationロジックをサードパーティ製のo9nに移行

--- src/assets/js/helper/getOrientation.ts
+++ src/assets/js/helper/getOrientation.ts
@@ -2,53 +2,24 @@ import { debounce } from 'throttle-debounce'
 
 import EL from '../constant/elements'
 
+const getOrientation = require('o9n').getOrientation
+
 /**
  * deviceの回転の向きを取得して<html>のデータ属性にセットします
- * @link https://wemo.tech/402 参考
  */
 export default (): void => {
-  let isReverse = false
+  const isLandscapeCheck = () => getOrientation().angle !== 0
 
-  // 正面設定が通常の場合の縦横判定処理
-  const isLandscapeCheck = () => window.orientation !== 0
-
-  // 正面設定が逆の場合の縦横判定処理
-  const isLandscapeCheckReverse = () => window.orientation === 0
-
-  // Androidなら正面設定を確認
-  if (EL.HTML.dataset.os === 'android') {
-    // @ts-ignore
-    const orientation =
-      screen.orientation || screen.mozOrientation || screen.msOrientation
-    if (
-      orientation.type === 'portrait-secondary' ||
-      orientation.type === 'landscape-primary'
-    ) {
-      isReverse = true
-    }
+  const checkOrientation = () => {
+    EL.HTML.dataset.orientation =
+      isLandscapeCheck() === true ? 'landscape' : 'portrait'
   }
 
-  /**
-   * chkorientation
-   * @param {boolean} isReverse
-   */
-  const chkorientation = (isReverse: boolean) => {
-    if (isReverse) {
-      // 正面が逆の場合
-      EL.HTML.dataset.orientation =
-        isLandscapeCheckReverse() === true ? 'landscape' : 'portrait'
-    } else {
-      // 正面が通常の場合
-      EL.HTML.dataset.orientation =
-        isLandscapeCheck() === true ? 'landscape' : 'portrait'
-    }
-  }
-
-  chkorientation(isReverse)
+  checkOrientation()
 
   window.addEventListener(
     'orientationchange',
-    debounce(150, () => chkorientation(isReverse)),
+    debounce(150, () => checkOrientation()),
     false
   )
 }
